527fc7d99b1a9ddee76fd2d3a60492cbc0761a3b
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-11-30 18:16:52 +0100

    Paso 2: Clase Pelota Pong

--- pingPong.py
+++ pingPong.py
@@ -1,4 +1,4 @@
-# pong_1_1.py: Ventana de juego
+# pong_1_2.py: Clase PelotaPong
 
 import pygame
 from pygame.locals import QUIT
@@ -8,6 +8,31 @@ VENTANA_HORI = 800  # Ancho de la ventana
 VENTANA_VERT = 600  # Alto de la ventana
 FPS = 60  # Fotogramas por segundo
 BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
+
+
+
+class PelotaPong:
+    def __init__(self, fichero_imagen):
+        # --- Atributos de la Clase ---
+
+        # Imagen de la Pelota
+        self.imagen = pygame.image.load(fichero_imagen).convert_alpha()
+
+        # Dimensiones de la Pelota
+        self.ancho, self.alto = self.imagen.get_size()
+
+        # Posición de la Pelota
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+
+        # Dirección de movimiento de la Pelota
+        self.dir_x = random.choice([-5, 5])
+        self.dir_y = random.choice([-5, 5])
+
+    def mover(self):
+        self.x += self.dir_x
+        self.y += self.dir_
+
 
 
 def main():
